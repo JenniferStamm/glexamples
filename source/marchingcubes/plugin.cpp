73c715834cb58420e407f9bf9d4e824aa53969e9
Jennifer Stamm <jenniferstamm1992@gmail.com>
2015-04-28 21:11:12 +0200

    Add MarchingCubes plugin by copying from EmptyExample

---
+++ source/marchingcubes/plugin.cpp
@@ -0,0 +1,15 @@
+#include <gloperate/plugin/plugin_api.h>
+
+#include "MarchingCubesPainter.h"
+
+#include <glexamples-version.h>
+
+GLOPERATE_PLUGIN_LIBRARY
+
+    GLOPERATE_PLUGIN(MarchingCubes
+    , "MarchingCubes"
+    , "Copy to implement a new example"
+    , GLEXAMPLES_AUTHOR_ORGANIZATION
+    , "v1.0.0" )
+
+GLOPERATE_PLUGIN_LIBRARY_END
